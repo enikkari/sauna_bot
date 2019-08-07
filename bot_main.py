5bf0793da943145ed9874fcd64b59749a9b35cdd
enik <eevanikkari@gmail.com>
2019-08-07 15:30:54 +0300

    Ping also helsinki flow

--- bot_main.py
+++ bot_main.py
@@ -7,20 +7,24 @@ from bot_logic import send_message_to_slack
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s %(name)s:%(lineno)d - %(message)s")
 logger = logging.getLogger(__name__)
 
+SLACK_TOKEN = os.environ["SLACK_TOKEN"]
 
-def main():
-    slack_token = os.environ["SLACK_TOKEN"]
-    channel_name = os.environ["CHANNEL_NAME"]
-    # bot_name = os.environ["BOT_NAME"]
 
-    send_message_to_slack(token=slack_token,
-                          channel_name=channel_name)
+def send_turn_on_sauna_message():
+    send_message_to_slack(token=SLACK_TOKEN,
+                          channel_name="hki-sauna",
+                          ping_channel=True)
+
+
+def send_general_info():
+    send_message_to_slack(token=SLACK_TOKEN,
+                          channel_name="helsinki",
+                          ping_channel=False)
 
 
 if __name__ == '__main__':
-    run_at = "15:30"
-    logger.info(f'Running time: {run_at}')
-    schedule.every().friday.at(run_at).do(main)
+    schedule.every().friday.at("12:12").do(send_general_info)
+    schedule.every().friday.at("15:28").do(send_turn_on_sauna_message)
     while True:
         schedule.run_pending()
         time.sleep(1)
